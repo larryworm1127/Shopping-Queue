dfe93129bd3659021dba33d6c4efc16ddc9bc8d3
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-06-14 01:05:01 -0400

    ported demo react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
+// Importing react-router-dom to use the React Router
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Importing the Queue and our simple Home Page
+import Queue from './components/Queue';
+import Home from './components/Home';
+
+class App extends React.Component {
+
+  state = {
+    abc: "123"
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={() => (<Home state={this.state}/>)}/>
+            <Route exact path='/queue' render={() => (<Queue state={this.state}/>)}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
