faf33ed41803957a098380dd200f9fb39ac8cf42
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-06-16 21:31:57 -0400

    updated navbar to use new styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,28 @@ import './App.css';
 // Importing the Queue and our simple Home Page
 import Queue from './components/Queue';
 import Home from './components/Home';
-import NavBar from "./components/navbar";
+import { createMuiTheme } from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+import indigo from "@material-ui/core/colors/indigo";
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
+
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: blue[900]
+    },
+    primary: {
+      main: indigo[700]
+    }
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '"Lato"',
+      'sans-serif'
+    ].join(',')
+  }
+});
 
 class App extends React.Component {
 
@@ -18,13 +39,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <NavBar/>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path='/' render={() => (<Home state={this.state}/>)}/>
-            <Route exact path='/queue' render={() => (<Queue state={this.state}/>)}/>
-          </Switch>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path='/' render={() => (<Home state={this.state}/>)}/>
+              <Route exact path='/queue' render={() => (<Queue state={this.state}/>)}/>
+            </Switch>
+          </BrowserRouter>
+        </ThemeProvider>
       </div>
     );
   }
