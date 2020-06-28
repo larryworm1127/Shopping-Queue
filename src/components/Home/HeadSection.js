14c32730101b9fcfe2f1e73b65ab298da88da3ea
pi-ukani <piyush.ukani@mail.utoronto.ca>
2020-06-28 12:21:29 -0400

    improved readability

--- src/components/Home/HeadSection.js
+++ src/components/Home/HeadSection.js
@@ -2,19 +2,17 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import {
-  Grid,
   Typography,
   Card,
   Button,
   Box,
   withStyles,
   withWidth,
-  isWidthUp
 } from "@material-ui/core";
 import { styles } from './style';
 
 function HeadSection(props) {
-  const { classes, width } = props;
+  const { classes } = props;
   return (
     <Fragment>
       <div className={classNames("main_div", classes.wrapper)}>
@@ -26,44 +24,35 @@ function HeadSection(props) {
               data-aos="zoom-in"
             >
               <div className={classNames(classes.containerFix, "container")}>
-                <Box justifyContent="space-between" className="row">
-                  <Grid item xs={12} md={5}>
-                    <Box
-                      display="flex"
-                      flexDirection="column"
-                      justifyContent="space-between"
-                      height="100%"
-                    >
-                      <Box mb={5}>
-                        <Typography
-                          variant={isWidthUp("lg", width) ? "h3" : "h4"}
-                        >
-                          Shopping Queue Manager
+
+                <div>
+                  <Typography
+                    variant={"h3"}
+                  >
+                    Shopping Queue Manager
                         </Typography>
-                      </Box>
-                      <div>
-                        <Box mb={2}>
-                          <Typography
-                            variant={isWidthUp("lg", width) ? "h6" : "body1"}
-                            color="textSecondary"
-                          >
-                            Now you don't need to risk your health for shopping
+                </div>
+                <div>
+                  <Box mb={2}>
+                    <Typography
+                      variant={"h6"}
+                      color="textSecondary"
+                    >
+                      Now you don't need to risk your health for shopping
                           </Typography>
-                        </Box>
-                        <Button
-                          variant="contained"
-                          color="secondary"
-                          fullWidth
-                          className={classes.extraLargeButton}
-                          classes={{ label: classes.extraLargeButtonLabel }}
-                          href="/register"
-                        >
-                          Signup
+                  </Box>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    fullWidth
+                    className={classes.extraLargeButton}
+                    classes={{ label: classes.extraLargeButtonLabel }}
+                    href="/register"
+                  >
+                    Signup
                         </Button>
-                      </div>
-                    </Box>
-                  </Grid>
-                </Box>
+                </div>
+
               </div>
             </Card>
           </Box>
@@ -75,7 +64,6 @@ function HeadSection(props) {
 
 HeadSection.propTypes = {
   classes: PropTypes.object,
-  width: PropTypes.string,
   theme: PropTypes.object
 };
 
