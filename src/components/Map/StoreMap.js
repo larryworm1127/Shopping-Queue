f0f15f23f724cb593fd284a86e60762591646eda
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-06-29 00:00:23 -0400

    added markers and side notes for map

--- src/components/Map/StoreMap.js
+++ src/components/Map/StoreMap.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import NavBar from '../Nav/navbar';
 import { withRouter } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
+import { stores } from '../../utils/stores';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 
-const mapStyles = {
-  // width: '70%',
-  height: '90%',
-};
+
+const styles = theme => ({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  mapStyles: {
+    position: 'absolute',
+    left: '1px',
+    top: '64px',
+    width: '100%',
+    height: '100%',
+  },
+  cardStyles: {
+    position: 'absolute',
+    right: 0,
+    top: '65px',
+    width: '30%',
+    height: '100%'
+  }
+});
 
 
 class StoreMap extends React.Component {
@@ -16,15 +50,16 @@ class StoreMap extends React.Component {
   state = {
     showingInfoWindow: false,  //Hides or the shows the infoWindow
     activeMarker: {},          //Shows the active marker upon click
-    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+    selectedPlace: {},         //Shows the infoWindow to the selected place upon a marker
   };
 
-  onMarkerClick = (props, marker, e) =>
+  onMarkerClick = (props, marker, e) => {
     this.setState({
-      selectedPlace: props,
+      selectedPlace: stores[props.id],
       activeMarker: marker,
       showingInfoWindow: true
     });
+  };
 
   onClose = props => {
     if (this.state.showingInfoWindow) {
@@ -35,8 +70,69 @@ class StoreMap extends React.Component {
     }
   };
 
+  handleHighlight = (index) => {
+    this.setState({
+      selectedPlace: stores[index],
+
+    });
+  };
+
+  renderMarkers = () => {
+    return stores.map((store, index) => (
+        <Marker
+          key={index}
+          id={index}
+          position={{
+            lat: store.coordinate[0],
+            lng: store.coordinate[1]
+          }}
+          onClick={this.onMarkerClick}
+        />
+      )
+    );
+  };
+
+  renderStoreCards = (classes) => {
+    return stores.map((store, index) => (
+        <Card key={index} id={index}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {store.type}
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {store.name}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {store.address}
+            </Typography>
+            <Typography variant="body2" component="p">
+              Open from <b>{store.openingTime}</b> to <b>{store.closingTime}</b>
+              <br/>
+              <b>Customer limit:</b> {store.customerLimit}
+              <br/>
+              <b>Customer Shop Time Limit:</b> {store.customerShopTime} min
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary">Queue Here</Button>
+            <Button
+              type="button"
+              color="primary"
+              onClick={() => {
+                this.handleHighlight(index);
+              }}
+            >
+              Highlight on Map
+            </Button>
+          </CardActions>
+        </Card>
+      )
+    );
+  };
+
   render() {
     const {
+      classes,
       location
     } = this.props;
 
@@ -46,27 +142,28 @@ class StoreMap extends React.Component {
         <CssBaseline/>
 
         <Grid container>
-          <Grid item xs={8}>
+          <Grid item xs={9} className={classes.mapStyles}>
             <Map
               google={this.props.google}
               zoom={16}
-              style={mapStyles}
-              initialCenter={{ lat: 43.66, lng: -79.39 }}
+              initialCenter={{ lat: 43.662410, lng: -79.395424 }}
             >
-              <Marker
-                onClick={this.onMarkerClick}
-                name={'Kenyatta International Convention Centre'}
-              />
+              {this.renderMarkers()}
               <InfoWindow
                 marker={this.state.activeMarker}
                 visible={this.state.showingInfoWindow}
                 onClose={this.onClose}
               >
                 <div>
-                  <h4>{this.state.selectedPlace.name}</h4>
+                  <h4>Store Name: {this.state.selectedPlace.name}</h4>
+                  <h4>Address: {this.state.selectedPlace.address}</h4>
                 </div>
               </InfoWindow>
             </Map>
+          </Grid>
+
+          <Grid item xs={3} className={classes.cardStyles}>
+            {this.renderStoreCards(classes)}
           </Grid>
         </Grid>
       </React.Fragment>
@@ -76,4 +173,4 @@ class StoreMap extends React.Component {
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyAgAnqHQF4Z6NpppDIGCA1gYD367qOPKFs'
-})(withRouter(StoreMap));
+})(withRouter(withStyles(styles)(StoreMap)));
---
+++ src/utils/stores.js
@@ -0,0 +1,71 @@
+const Store = function (
+  name,
+  address,
+  coordinate,
+  type,
+  openingTime,
+  closingTime,
+  customerLimit,
+  customerShopTime
+) {
+  this.name = name;
+  this.address = address;
+  this.coordinate = coordinate;
+  this.type = type;
+  this.openingTime = openingTime;
+  this.closingTime = closingTime;
+  this.customerLimit = customerLimit;
+  this.customerShopTime = customerShopTime;
+};
+
+
+const StoreTypes = {
+  GROCERY: 'Grocery',
+  DEPARTMENT: 'Department',
+  CLOTHING: 'Clothing',
+  ACCESSORY: 'Accessory',
+};
+
+
+export const stores = [
+  new Store(
+    'Floor Mart',
+    '123 Street',
+    [43.658702, -79.397168],
+    StoreTypes.CLOTHING,
+    '10:00',
+    '18:30',
+    10,
+    30
+  ),
+  new Store(
+    'Shoppers Not Drug Mart',
+    '456 Street',
+    [43.660896, -79.385397],
+    StoreTypes.DEPARTMENT,
+    '11:00',
+    '19:30',
+    15,
+    50
+  ),
+  new Store(
+    'Yes Frills',
+    '789 Street',
+    [43.658662, -79.390168],
+    StoreTypes.GROCERY,
+    '08:00',
+    '20:30',
+    30,
+    40
+  ),
+  new Store(
+    'Unfreshco',
+    '000 Street',
+    [43.661915, -79.379381],
+    StoreTypes.GROCERY,
+    '07:00',
+    '21:00',
+    20,
+    30
+  ),
+];
