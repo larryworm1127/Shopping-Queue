19b55042cf18c4eb52a22ac91c62180431b3205b
Aaditya Dave <anb.dave@gmail.com>
2020-08-05 21:22:00 -0400

    Complete shopper model.

--- models/shopper.js
+++ models/shopper.js
@@ -1,29 +1,38 @@
 const mongoose = require('mongoose');
+const Owner = require('./owner');
 
 const User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true,
+    trim: true
+  },
   firstName: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   lastName: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   address: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
-  email: {
-    type: String,
-    required: true
-  },
+  // email: {
+  //   type: String,
+  //   required: true
+  // },
   remindTime: {
     type: Number,
     required: true
-  }
-  // currentQueue,
-  // favouriteStores,
-  // searchHistory,
-  // queueHistory
+  },
+  favouriteStores: [{type: mongoose.Schema.Types.ObjectId, ref: 'Owner'}],
+  searchHistory: [{type: mongoose.Schema.Types.ObjectId, ref: 'Owner', searchDate: {type: String, required: true}}],
+  queueHistory: [{type: mongoose.Schema.Types.ObjectId, ref: 'Owner', searchDate: {type: String, required: true}}]
 });
 
+module.exports = { User };
