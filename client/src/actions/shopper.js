b5940837ce9ac77de6022efa40e3f2d72ddd4a16
Aaditya Dave <anb.dave@gmail.com>
2020-08-11 19:46:46 -0400

    Fix requests to work with front-end.

--- client/src/actions/shopper.js
+++ client/src/actions/shopper.js
@@ -1,7 +1,7 @@
 // Functions to help with shopper actions.
 
 export const getShopperProfile = (username, profileComp) => {
-  const url = `/api/shopper/${username}`;
+  const url = `/api/shopper/profile/${username}`;
 
   fetch(url)
     .then(res => {
@@ -11,12 +11,13 @@ export const getShopperProfile = (username, profileComp) => {
     })
     .then(json => {
       if (json) {
+        console.log(json);
         profileComp.setState({
           firstName: json.firstName,
           lastName: json.lastName,
           email: json.email,
           address: json.address,
-          favouriteStores: json.favouriteStores,
+          favoriteStores: json.favouriteStores,
           remindTime: json.remindTime
         });
       }
@@ -28,7 +29,7 @@ export const getShopperProfile = (username, profileComp) => {
 
 
 export const updateShopperProfile = (username, profileComp) => {  
-  const request = new Request(`/api/profile`, {
+  const request = new Request(`/api/shopper/profile/${username}`, {
     method: 'PATCH',
     body: JSON.stringify(profileComp.state),
     headers: {
@@ -36,7 +37,7 @@ export const updateShopperProfile = (username, profileComp) => {
       'Content-Type': 'application/json'
     }
   });
-
+  console.log(profileComp.state);
   fetch(request)
     .then(res => {
       if (res.status === 200) {
@@ -50,7 +51,6 @@ export const updateShopperProfile = (username, profileComp) => {
           lastName: json.lastName,
           email: json.email,
           address: json.address,
-          favouriteStores: json.favouriteStores,
           remindTime: json.remindTime,
           edit: false
         });
