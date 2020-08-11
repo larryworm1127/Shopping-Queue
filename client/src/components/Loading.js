8fef05a35a443eb5f0632fb7e2b0a9b5e95f4872
Le Rong Shi <larry.shi@mail.utoronto.ca>
2020-08-10 21:07:18 -0400

    added better looking loading page

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core';
+
+
+const styles = theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+});
 
 
 class Loading extends React.Component {
 
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        Loading ...
-      </div>
+      <Backdrop className={classes.backdrop} open>
+        <CircularProgress color="inherit"/>
+      </Backdrop>
     );
   }
 }
 
-export default Loading;
+export default withStyles(styles)(Loading);
